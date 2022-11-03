f79a1f2dd4a7f70fbc4477935494648e9fbcc11a
Oliver Roots <oliver.roots@gmail.com>
2022-11-03 20:14:36 +0200

    added price input, not working in combination with brand input

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 This is Angular study with Hasan. 
 
+03/11/2022 II
+
+added price input box.
+works separately but not as combined
 
 03/11/2022
 
 First Commit
+
 		 
 
--- src/app/multiple-api/multiple-api.component.ts
+++ src/app/multiple-api/multiple-api.component.ts
@@ -8,29 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultipleAPIComponent implements OnInit {
   constructor(public http: HttpClient) {}
-  product: any;
+  productsHtml: any;
   selectedProduct: any;
   filteredString: any = '';
+  filteredPrice: any;
   realList: any;
   ngOnInit(): void {
     this.http.get('https://dummyjson.com/products').subscribe((data: any) => {
-      console.log(data.products);
-      this.product = data.products; //all products
+      //console.log(data.products);
+      this.productsHtml = data.products; //all products
       this.realList = data.products;
     });
   }
-  inputChange(event: any) {
-    console.log(event);
+  inputChangeBrand(event: any) {
+    console.log(event, this.filteredString);
     let x = event.toLowerCase();
     let products = [];
-    for (const product of this.realList) {
-      let brand = product.brand;
-      brand = brand.toLowerCase();
-      if (brand.includes(x)) {
-        products.push(product);
+    if (typeof this.filteredPrice === 'undefined') {
+      console.log('yes');
+      for (const product of this.realList) {
+        let brand = product.brand;
+        brand = brand.toLowerCase();
+        if (brand.includes(x)) {
+          products.push(product);
+        }
+      }
+    } else {
+      for (const product of this.productsHtml) {
+        let brand = product.brand;
+        brand = brand.toLowerCase();
+        if (brand.includes(x)) {
+          products.push(product);
+        }
       }
     }
     //results
-    this.product = products;
+    this.productsHtml = products;
+  }
+  inputChangePrice(event: any) {
+    let products = [];
+    if (this.filteredString === '') {
+      console.log(event);
+      for (const product of this.realList) {
+        if (event <= product.price) {
+          products.push(product);
+        }
+      }
+    } else {
+      for (const product of this.productsHtml) {
+        if (event <= product.price) {
+          products.push(product);
+        }
+      }
+    }
+    //results
+    this.productsHtml = products;
   }
 }
